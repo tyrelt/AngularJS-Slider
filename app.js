3c0ac592d9489b24e50109a37f95e65c26233742
david-davidson <david.davidson@logos.com>
2014-05-10 19:43:29 -0700

    Cleaned up the new slider directive a little

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-// Set up the sliderApp namespace--no need for routing here
+// Set up the sliderApp namespace--no need for routing here, really
 var sliderApp = angular.module('sliderApp', []);
 
-// Set up our slider as a directive: a reusable, standalone component
-sliderApp.directive('slider', function() {
+// Set up our slider as a standalone custom directive, so it's reusable
+// (Note the camel cased "mySlider" vs., in index.html, <my-slider>: intentional)
+sliderApp.directive('mySlider', function() {
     return {
-        templateUrl: 'views/sliderView.html'
+        restrict: 'E', // So we can invoke it as a standalone element
+        templateUrl: 'views/sliderView.html',
+        controller: 'sliderController'
     };
 });
